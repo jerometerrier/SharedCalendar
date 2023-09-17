43d011f918c4004545304845e49b324076de7ff3
Jérôme Terrier <jerome+git@jelosat.com>
2023-09-17 20:26:09 +0200

    add class CalendrierGarde + deleteevents

--- src/main.py
+++ src/main.py
@@ -12,17 +12,16 @@ from googleapiclient.errors import HttpError
 # If modifying these scopes, delete the file token.json.
 
 
+def is_date_format(date):
+	try:
+		datetime.datetime.strptime(date, '%d/%m/%Y')
+		return True
+	except ValueError:
+		print("Incorrect data format, should be DD/MM/YYYY")
+		return False
 
 
 class CalendarGoogle:
-	@staticmethod
-	def is_date_format(date):
-		try:
-			datetime.datetime.strptime(date, '%d/%m/%Y')
-			return True
-		except ValueError:
-			print("Incorrect data format, should be DD/MM/YYYY")
-			return False
 		
 	def __init__(self):
 		self.SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -67,23 +66,22 @@ class CalendarGoogle:
 		self.IDCalendar = [cal['id'] for cal in self.calendar_list['items'] if cal['summary'] == self.CalName][0]
 		if self.IDCalendar == '':
 			print('No calendar found')
-	def GetEvents(self, from_date, to_date):
+	def GetEvents(self, start_date, end_date):
 		self.events = []
-		if not self.is_date_format(from_date) or not self.is_date_format(to_date):
+		if not is_date_format(start_date) or not is_date_format(end_date):
 			return
-		from_date = datetime.datetime.strptime(from_date, '%d/%m/%Y').isoformat() + 'Z'
-		to_date = datetime.datetime.strptime(to_date, '%d/%m/%Y').isoformat() + 'Z'
+		start_date = datetime.datetime.strptime(start_date, '%d/%m/%Y').isoformat() + 'Z'
+		end_date = datetime.datetime.strptime(end_date, '%d/%m/%Y').isoformat() + 'Z'
 
-		if to_date<from_date:
+		if end_date<start_date:
 			print('to_date must be after from_date')
 			return
-		from_date = datetime.datetime.utcnow().isoformat() + 'Z'
 		if self.IDCalendar == '':
 			print('No calendar found')
 			print('Please use GetIDCalendar()')
 		else:
-			self.events_result = self.service.events().list(calendarId=self.IDCalendar, timeMin=from_date,
-											timeMax=to_date, singleEvents=True,
+			self.events_result = self.service.events().list(calendarId=self.IDCalendar, timeMin=start_date,
+											timeMax=end_date, singleEvents=True,
 												orderBy='startTime').execute()
 			self.events_items = self.events_result.get('items', [])
 			if not self.events_items:
@@ -100,7 +98,9 @@ class CalendarGoogle:
 					return
 
 				start = start.strftime('%d/%m/%Y %H:%M')
-				self.events.append((start, event['summary']))
+				self.events.append({'start' : start,
+						'summary': event['summary'],
+						 'id': event['id']})
 				# print(f'{self.CalName} : {start} ==> {event["summary"]}')
 	def CreateEvent(self, name, start, end, description = ''):
 		event = {
@@ -108,11 +108,11 @@ class CalendarGoogle:
 			'location': '',
 			'description': description,
 			'start': {
-				'dateTime': start,
+				'date': start,
 				'timeZone': 'Europe/Paris',
 			},
 			'end': {
-				'dateTime': end,
+				'date': end,
 				'timeZone': 'Europe/Paris',
 			},
 			'recurrence': [
@@ -123,31 +123,161 @@ class CalendarGoogle:
 	 				},
 					 
 			],
-			'reminders': {
-				'useDefault': False,
-				'overrides': [
-					{'method': 'email', 'minutes': 24 * 60},
-					{'method': 'popup', 'minutes': 10},
-				],
-			},
+			# 'reminders': {
+			# 	'useDefault': False,
+			# 	'overrides': [
+			# 		{'method': 'email', 'minutes': 24 * 60},
+			# 		{'method': 'popup', 'minutes': 10},
+			# 	],
+			# },
 		}
 		event = self.service.events().insert(calendarId=self.IDCalendar, body=event).execute()
 		print('Event created: %s' % (event.get('htmlLink')))
+	def DeleteEvent(self, event_id):
+		self.service.events().delete(calendarId=self.IDCalendar, eventId=event_id).execute()
+		print('Event deleted')
+	def DeleteAllEvents(self, start_date, end_date, name):
+		self.GetEvents(start_date, end_date)
+		for event in self.events:
+			if event['summary'] == name:
+				print(f"{event['summary']} : {event['start']}")
+				self.DeleteEvent(event['id'])
 
 	
 
+
+
+class CalendrierGarde:
+	def __init__(self):
+		self.first_day = None
+		self.Day1 = None
+		self.Day2 = None
+		self.Day3 = None
+		self.Day4 = None
+		self.Day5 = None
+		self.Day6 = None
+		self.Day7 = None
+		self.Day8 = None
+		self.Day9 = None
+		self.Day10 = None
+		self.Day11 = None
+		self.Day12 = None
+		self.Day13 = None
+		self.Day14 = None
+
+		pass
+	def DeuxTroisDeux(self, first_day, calendar1, calendar2):
+		if not is_date_format(first_day):
+			return
+		first_day = datetime.datetime.strptime(first_day, '%d/%m/%Y')
+		self.Day1 = first_day
+		self.Day2 = first_day + datetime.timedelta(days=1)
+		self.Day3 = first_day + datetime.timedelta(days=2)
+		self.Day4 = first_day + datetime.timedelta(days=3)		
+		self.Day5 = first_day + datetime.timedelta(days=4)
+		self.Day6 = first_day + datetime.timedelta(days=5)
+		self.Day7 = first_day + datetime.timedelta(days=6)
+		self.Day8 = first_day + datetime.timedelta(days=7)
+		self.Day9 = first_day + datetime.timedelta(days=8)
+		self.Day10 = first_day + datetime.timedelta(days=9)
+		self.Day11 = first_day + datetime.timedelta(days=10)
+		self.Day12 = first_day + datetime.timedelta(days=11)
+		self.Day13 = first_day + datetime.timedelta(days=12)
+		self.Day14 = first_day + datetime.timedelta(days=13)
+
+
+		self.Day1Cal = calendar1
+		self.Day2Cal = calendar1
+		self.Day3Cal = calendar2
+		self.Day4Cal = calendar2
+		self.Day5Cal = calendar2
+		self.Day6Cal = calendar1
+		self.Day7Cal = calendar1
+		self.Day8Cal = calendar2
+		self.Day9Cal = calendar2
+		self.Day10Cal = calendar1
+		self.Day11Cal = calendar1
+		self.Day12Cal = calendar1
+		self.Day13Cal = calendar2
+		self.Day14Cal = calendar2
+	
+	def showCalendrier(self):
+		print(f"Jour 1 : {self.Day1}")
+		print(f"Jour 2 : {self.Day2}")
+		print(f"Jour 3 : {self.Day3}")
+		print(f"Jour 4 : {self.Day4}")
+		print(f"Jour 5 : {self.Day5}")
+		print(f"Jour 6 : {self.Day6}")
+		print(f"Jour 7 : {self.Day7}")
+		print(f"Jour 8 : {self.Day8}")
+		print(f"Jour 9 : {self.Day9}")
+		print(f"Jour 10 : {self.Day10}")
+		print(f"Jour 11 : {self.Day11}")
+		print(f"Jour 12 : {self.Day12}")
+		print(f"Jour 13 : {self.Day13}")
+		print(f"Jour 14 : {self.Day14}")
+
+	def createDefaultEvent(self):
+		# Créez une liste pour stocker les variables Day1Cal à Day14Cal
+		calendar_list = [
+			(self.Day1Cal, self.Day1),(self.Day2Cal, self.Day2),
+			(self.Day3Cal, self.Day3),(self.Day4Cal, self.Day4),
+			(self.Day5Cal, self.Day5),(self.Day6Cal, self.Day6),
+			(self.Day7Cal, self.Day7),(self.Day8Cal, self.Day8),
+			(self.Day9Cal, self.Day9),(self.Day10Cal, self.Day10),
+			(self.Day11Cal, self.Day11),(self.Day12Cal, self.Day12),
+			(self.Day13Cal, self.Day13),(self.Day14Cal, self.Day14)]
+
+		if any(cal[0] is None for cal in calendar_list):
+			print("Please set calendar")
+
+		for cal in calendar_list:
+			start_date = cal[1].strftime('%Y-%m-%d')
+			end_date = (cal[1] + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
+
+			cal[0].CreateEvent('Garde', start_date, end_date, 'Garde alternee')
+	
+	def checkIntregrity(self):
+		pass
+		
+		
+
+	
 def main():
 	CalJerome = CalendarGoogle()
 	CalJerome.GetListCalendar()
 	CalJerome.GetIDCalendar('Garde Alternee')
-	CalJerome.GetEvents("01/09/2023", "30/09/2023")
+	CalJerome.GetEvents("01/09/2023", "18/09/2023")
 	CalElise = CalendarGoogle()
 	CalElise.GetListCalendar()
 	CalElise.GetIDCalendar('Garde Elise')
 	# CalElise.CreateEvent('Test2', '2023-09-18T10:00:00+02:00', '2023-09-18T11:00:00+02:00', 'Description test')
-	CalElise.GetEvents("01/09/2023", "30/09/2023")
-	print(CalJerome.events_result)
+	CalElise.GetEvents("01/09/2023", "18/09/2023")
+	# print(CalJerome.events)
+	# CalElise.DeleteAllEvents("01/09/2023", "18/09/2023", "Garde")
+	calendrier = CalendrierGarde()
+	calendrier.DeuxTroisDeux("01/09/2023", CalJerome, CalElise)
+	# calendrier.showCalendrier()
+	calendrier.createDefaultEvent()
 
 
+
+
+def init_calendar(start_date, end_date):
+	if not is_date_format(start_date) or not is_date_format(end_date):
+		return
+	start_date = datetime.datetime.strptime(start_date, '%d/%m/%Y')#.isoformat() + 'Z'
+	start_date_iso = start_date.isoformat() + 'Z'
+	end_date = datetime.datetime.strptime(end_date, '%d/%m/%Y')#.isoformat() + 'Z'
+	end_date_iso = end_date.isoformat() + 'Z'
+
+	# Calcul de la plage de dates
+	date_range = [start_date + datetime.timedelta(days=i) for i in range((end_date - start_date).days + 1)]
+
+	# Afficher la plage de dates
+	for date in date_range:
+		print(date.date())
+	
 if __name__ == '__main__':
     main()
+	# init_calendar("01/09/2023", "18/09/2023")
